03479113f156814afb180dd274601ba17009e63c
pavithra1708 <pavithra.apv@gmail.com>
2017-08-07 06:10:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,13 @@
 console.log('Loaded!');
 
+
+//change value
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+//move image
+var img=document.getElementById('img');
+img.onclick=function()
+{
+  img.style.marginleft='100px';  
+};
